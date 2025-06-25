fd34977b52051325e3a7310ada392ea9499b5ab4
Crystal Reinheart 055 <crystalhuang881@gmail.com>
2025-06-25 07:29:04 +0700

    connect four

--- Seed.java
+++ Seed.java
@@ -14,45 +14,6 @@ import javax.swing.ImageIcon;
  * Ideally, we should define two enums with inheritance, which is,
  *  however, not supported.
  */
-
-
 public enum Seed {   // to save as "Seed.java"
     CROSS, NOUGHT, NO_SEED
 }
-
-
-//
-//public enum Seed {   // to save as "Seed.java"
-//    CROSS("X", "images/x _tictactoe.png"),   // displayName, imageFilename
-//    NOUGHT("O", "images/o_tictactoe.png"),
-//    NO_SEED(" ", null);
-//
-//    // Private variables
-//    private String displayName;
-//    private Image img = null;
-//
-//    // Constructor (must be private)
-//    private Seed(String name, String imageFilename) {
-//        this.displayName = name;
-//
-//        if (imageFilename != null) {
-//            URL imgURL = getClass().getClassLoader().getResource(imageFilename);
-//            ImageIcon icon = null;
-//            if (imgURL != null) {
-//                icon = new ImageIcon(imgURL);
-//                //System.out.println(icon);  // debugging
-//            } else {
-//                System.err.println("Couldn't find file " + imageFilename);
-//            }
-//            img = icon.getImage();
-//        }
-//    }
-//
-//    // Public getters
-//    public String getDisplayName() {
-//        return displayName;
-//    }
-//    public Image getImage() {
-//        return img;
-//    }
-//}
